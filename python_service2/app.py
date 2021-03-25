20b34b85cbe58cd97277929d6e84bba91a42ba50
ben <moon791017@gmail.com>
2021-03-25 17:19:19 +0800

    aa

--- python_service2/app.py
+++ python_service2/app.py
@@ -2,6 +2,7 @@ import math
 import sys
 from flask import Flask, request, abort, render_template, jsonify, make_response
 from flask_cors import CORS
+from pytube import YouTube
 
 app = Flask(__name__)
 CORS(app)
@@ -19,7 +20,13 @@ CORS(app)
 @app.route('/hi2', methods=['GET'])
 def hi():
     return "hi2"
+@app.route('/download', methods=['GET'])
 
+def download():
+    YouTube('https://youtu.be/2lAe1cqCOXo').streams.first().download()
+    yt = YouTube('http://youtube.com/watch?v=2lAe1cqCOXo')
+    yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+    return "ok"
 
 if __name__ == "__main__":
     app.run()
